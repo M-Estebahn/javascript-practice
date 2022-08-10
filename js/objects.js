fbdcbd9c078ea1f95a61b12b1e22728e6ee0e420
M-Estebahn <molina.estebahn@gmail.com>
2022-08-10 14:11:13 -0500

    messing with some js objects

---
+++ js/objects.js
@@ -0,0 +1,65 @@
+(function() {
+    "use strict";
+
+    /**
+     * TODO:
+     * Create an object with firstName and lastName properties that are strings
+     * with your first and last name. Store this object in a variable named
+     * `person`.
+     *
+     * Example:
+     *  > console.log(person.firstName) // "Rick"
+     *  > console.log(person.lastName) // "Sanchez"
+     */
+        var person={
+            firstName:"Estebahn",
+            lastName:"Molina"
+    }
+    console.log(person.firstName)
+    console.log(person.lastName)
+    /**
+     * TODO:
+     * Add a sayHello method to the person object that returns a greeting using
+     * the firstName and lastName properties.
+     * console.log the returned message to check your work
+     *
+     * Example
+     * > console.log(person.sayHello()) // "Hello from Rick Sanchez!"
+     */
+    person.sayHello=function () {
+        console.log("Hello, "+person.firstName+" "+person.lastName);
+
+    }
+    person.sayHello()
+
+    /** TODO:
+     * HEB has an offer for the shoppers that buy products amounting to
+     * more than $200. If a shopper spends more than $200, they get a 12%
+     * discount. Write a JS program, using conditionals, that logs to the
+     * browser, how much Ryan, Cameron and George need to pay. We know that
+     * Cameron bought $180, Ryan $250 and George $320. Your program will have to
+     * display a line with the name of the person, the amount before the
+     * discount, the discount, if any, and the amount after the discount.
+     *
+     * Uncomment the lines below to create an array of objects where each object
+     * represents one shopper. Use a foreach loop to iterate through the array,
+     * and console.log the relevant messages for each person
+     */
+
+    var shoppers = [
+        {name: 'Cameron', amount: 180},
+        {name: 'Ryan', amount: 250},
+        {name: 'George', amount: 320}
+    ];
+    shoppers.forEach(function (shopper) {
+        let discount = shopper.amount*.12
+        if (shopper.amount < 200) {
+            console.log( shopper.name + ', you only spent $ ' + shopper.amount + ' and that does not qualify you for a discount')
+        }
+        else
+            console.log('Congratulations ' + shopper.name + ' you spent $' + shopper.amount + ' your discount is $' + discount + ' Your new total is $' + (shopper.amount - discount))
+    })
+
+
+
+})();
